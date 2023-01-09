6f09f0d44168d9712f292f740dd707eacdc5e1f7
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-09 18:35:04 +0100

    create Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar"> 
+            <img src="" alt="globe-icon" />
+            <h1> my travel journal </h1>
+        </nav>
+    )
+}
